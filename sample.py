55bbf95e724f1a31f93fdb49661edc4c99815adf
Damoon <damoon.shah@torontomu.ca>
2023-03-09 17:44:59 -0500

    Perfect format?

--- sample.py
+++ sample.py
@@ -1,2 +1,12 @@
-def foo(a):
+"""
+Helper functions live here
+"""
+
+
+def increment_by_two(var_to_increment):
+    """
+    This function increments by two
+    :param var_to_increment: param to increment
+    :return: incremented variable
+    """
     return a + 2
