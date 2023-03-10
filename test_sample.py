b390e6ae74a7e88a2b401659583989b4a57c2717
Damoon <damoon.shah@torontomu.ca>
2023-03-09 19:12:18 -0500

    Fixing the increment by three tests #3

--- test_sample.py
+++ test_sample.py
@@ -17,7 +17,8 @@ class TestSampleMethods(unittest.TestCase):
         self.assertEqual(increment_by_two(-2), 0)
         self.assertEqual(increment_by_two(0), 2)
         self.assertEqual(increment_by_two(3), 5)
-    
+
+
     def test_increment_by_three(self):
         """
         Test increments of 3
@@ -26,5 +27,6 @@ class TestSampleMethods(unittest.TestCase):
         self.assertEqual(increment_by_three(0), 3)
         self.assertEqual(increment_by_three(3), 6)
 
+        
 if __name__ == '__main__':
     unittest.main()
